809c52fd225c82ec1839886cbedfedad4d787c3e
Francisco Madrigal <francisco.madrigal@vipera.com>
2022-09-06 12:08:07 +0200

    Add Pet Clinic Index Page and Controller

---
+++ pet-clinic-web/src/main/java/tutorial/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package tutorial.sfgpetclinic.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class IndexController {
+
+    @RequestMapping({"","/", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
