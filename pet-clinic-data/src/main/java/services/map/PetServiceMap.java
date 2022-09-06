892fa9460780f18dcad6baf49474a090aaaf231e
Francisco Madrigal <francisco.madrigal@vipera.com>
2022-09-06 11:53:27 +0200

    Implement Map Based Services

---
+++ pet-clinic-data/src/main/java/services/map/PetServiceMap.java
@@ -0,0 +1,35 @@
+package services.map;
+
+import services.CrudService;
+import services.PetService;
+import tutorial.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet findByID(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+}
+
---
+++ pet-clinic-data/src/main/java/services/map/VetService.java
@@ -0,0 +1,33 @@
+package services.map;
+
+import services.CrudService;
+import tutorial.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public class VetService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet findByID(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+}
